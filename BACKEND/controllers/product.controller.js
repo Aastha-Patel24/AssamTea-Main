1a57bf03d49f7ee7a1baaea77cb4d253975d1012
DipalRana21 <rdipal993@gmail.com>
2025-06-19 21:18:28 +0530

    Update product.controller.js

--- BACKEND/controllers/product.controller.js
+++ BACKEND/controllers/product.controller.js
@@ -4,8 +4,8 @@ export const createProduct = async (req,res)=>{
     try {
         const { name,image,price,category,stock,ratingStars,description,noOfRatings, availableSizes,productLife,instructions,temperature}=req.body;
 
-        if (!name || !image || !price || !category) {
-      return res.status(400).json({ message: "Name, image,price, and category are required" });
+        if (!name || !image || !price ) {
+      return res.status(400).json({ message: "Name, image,price are required" });
     }
 
     const product = await Product.create({
@@ -60,3 +60,41 @@ export const getProductById= async(req,res)=>{
           return res.status(500).json({ success: false, message: 'Server error', error: error.message });
     }
 }
+
+
+export const updateProduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const updates = req.body;
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      updates,
+      { new: true } // Return the updated document
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Product updated successfully",
+      product: updatedProduct,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Product.findByIdAndDelete(id);
+    return res.status(200).json({ success: true, message: "Product deleted successfully" });
+  } catch (err) {
+    return res.status(500).json({ success: false, message: "Error deleting product" });
+  }
+};
