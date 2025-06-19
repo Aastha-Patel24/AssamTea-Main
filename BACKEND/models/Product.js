d54ff0dcdb736b0ca9c4cd82c4f7297f26ffd6ef
DipalRana21 <rdipal993@gmail.com>
2025-06-19 21:17:27 +0530

    Update Product.js

--- BACKEND/models/Product.js
+++ BACKEND/models/Product.js
@@ -21,7 +21,7 @@ const productSchema = new mongoose.Schema({
     category: {
         type: String,
         enum: ['Black Tea', 'Green Tea', 'Herbal Tea'],
-        required: true,
+       
     },
 
     stock: {
