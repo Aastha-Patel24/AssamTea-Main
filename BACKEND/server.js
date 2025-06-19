bab3ff05e6b958b921c73e1239b161a5db9b2d96
DipalRana21 <rdipal993@gmail.com>
2025-06-19 21:16:50 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -18,8 +18,11 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json());
+// app.use(express.json());
 app.use(cookieParser());
+
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.get('/', (req, res) => {
     res.send('Assam Tea Backend Running Successfully')
